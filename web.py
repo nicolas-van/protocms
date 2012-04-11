75ed053c77d7a93909b7d511e73db5182243461a
Nicolas V <niv@niv-hp5320m.(none)>
2012-04-11 18:35:22 +0200

    Just the same as previously, but simpler

--- web.py
+++ web.py
@@ -5,11 +5,7 @@ app.debug = True
 
 def route(*args, **kwargs):
     def wrapper(fct):
-        @app.route(*args, **kwargs)
-        @db.transactionnal
-        def wrapper2(*args2, **kwargs2):
-            return fct(*args2, **kwargs2)
-        return wrapper2
+        return app.route(*args, **kwargs)(db.transactionnal(fct))
     return wrapper
 
 @route("/")

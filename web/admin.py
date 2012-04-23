3c7f5f52d60773e709279056eb9c2ed4d6b3aff1
Nicolas V <niv@niv-hp5320m.(none)>
2012-04-23 16:15:23 +0200

    Added setup for json-rpc

--- web/admin.py
+++ web/admin.py
@@ -3,13 +3,47 @@ import web
 import flask
 import os
 import os.path
+from flask import json
 
 @web.route("/admin")
 def admin():
     return flask.redirect("/admin/index.html")
 
+rpc_methods = {}
+
+def rpc(fct):
+    rpc_methods[fct.__name__] = fct
+    return fct
+
+@web.route("/adminapi", methods=["POST"])
+def admin_api():
+    data = flask.request.json
+    fct = rpc_methods[data["method"]]
+    params = data["params"]
+    try:
+        res = fct(*params.get("args", []), **params.get("kwargs", {}))
+        return flask.jsonify(**{
+            "jsonrpc": "2.0",
+            "result": res,
+            "id": data["id"],
+        })
+    except Exception as e:
+        import traceback
+        return flask.jsonify(**{
+            "jsonrpc": "2.0",
+            "error": {
+                "traceback": traceback.format_exc(),
+                "message": unicode(e)
+            },
+            "id": data["id"],
+        })
+
 @web.route("/admin/<path:file_>")
 def admin_static(file_):
     path = os.path.join(os.path.dirname(__file__), "adminstatic", file_)
     return flask.send_file(path)
+
+@rpc
+def hello():
+    return "hello"
 
--- web/adminstatic/admin.js
+++ web/adminstatic/admin.js
@@ -20,6 +20,44 @@ admin.Admin = admin.Widget.extend({
     template: admin.templateEngine.admin,
 });
 
+var json_rpc = function(url, fct_name, params, settings) {
+    var data = {
+        jsonrpc: "2.0",
+        method: fct_name,
+        params: params,
+        id: Math.floor(Math.random()* (1000*1000*1000)),
+    };
+    return $.ajax(url, _.extend({}, settings, {
+        url: url,
+        dataType: 'json',
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+    })).pipe(function(result) {
+        if (result.error !== undefined) {
+            return $.Deferred().reject("server", result.error);
+        }
+        else
+            return result.result;
+    }, function() {
+        var def = $.Deferred();
+        return def.reject.apply(def, ["communication"].concat(_.toArray(arguments)));
+    });
+};
+
+admin.Session = nova.Class.extend({
+    call: function(fct_name, args, kwargs) {
+        return json_rpc("../adminapi", fct_name, {args: args || [], kwargs: kwargs || {}});
+    },
+});
+
+var ses = new admin.Session();
+ses.call("hello").then(function() {
+    console.log("everything is fine", _.toArray(arguments));
+}, function() {
+    console.log("everything goes bad", _.toArray(arguments));
+});
+
 });
 })();
 
